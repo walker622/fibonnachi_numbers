2a7d80b8404a8421cb853c509570963ffc260d9a
walker622 <dmitro.ivanov96@gmail.com>
2020-01-03 02:18:52 +0300

    Fib recursive algorithm

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,7 @@
 object Main {
 
     fun fib(n: Int): Int {
-        if(n <= 1)
+        if (n <= 1)
             return n
 
         val digit1 = fib(n - 1)
@@ -10,8 +10,48 @@ object Main {
         return digit1 + digit2
     }
 
+    fun fibDp(n: Int, memo: IntArray): Int {
+        if (memo[n - 1] != -1) {
+            return memo[n - 1]
+        }
+
+        if (n <= 1)
+            return n
+
+        memo[n - 1] = fib(n - 1) + fib(n - 2)
+        return memo[n - 1]
+    }
+
+    fun fibOptimized(n: Int): Int {
+        val arr = IntArray(n + 2)
+        arr[0] = 0
+        arr[1] = 1
+
+        for(i in 2..n) {
+            arr[i] = arr[i - 1] + arr[i - 2]
+        }
+
+        return arr[n]
+    }
+
     @JvmStatic
     fun main(vararg args: String) {
-        println(fib(5))
+        var start = System.nanoTime()
+        println(fib(30))
+        var end = System.nanoTime()
+        println("first algo: ${end - start}")
+
+        start = System.nanoTime()
+        println(fibDp(30, IntArray(30) {
+            -1
+        }))
+        end = System.nanoTime()
+        println("second algo: ${end - start}")
+
+        start = System.nanoTime()
+        println(fibOptimized(45))
+        end = System.nanoTime()
+        println("third algo: ${end - start}")
+
     }
 }
