1bd098c6f60519ee955d5bd2367dba1ad42e0e47
walker622 <dmitro.ivanov96@gmail.com>
2020-01-03 01:56:32 +0300

    Fib recursive algorithm

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,17 @@
 object Main {
 
+    fun fib(n: Int): Int {
+        if(n <= 1)
+            return n
+
+        val digit1 = fib(n - 1)
+        val digit2 = fib(n - 2)
+
+        return digit1 + digit2
+    }
+
     @JvmStatic
     fun main(vararg args: String) {
-
+        println(fib(5))
     }
 }
